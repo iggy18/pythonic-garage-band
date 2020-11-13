0a027937c67c625db9b0206a4b124114995e7559
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2020-11-12 21:26:20 -0800

    17 passing tests

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -6,6 +6,14 @@ class Band:
         return f"The band {self.name}"
     def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+            # could also use 'solos += [member.play_solo()]'
+        return solos
+
+
 
 class Musician:
     def __init__(self, name):
@@ -14,33 +22,31 @@ class Musician:
         return f"My name is {self.name} and I play {self.get_instrument()}"
     def get_instrument(self):
         return f"bleh"
+    def __repr__(self):
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
 
 
 class Guitarist(Musician):
-    def __init__(self, name):
+    def __init__(self, name): 
         self.name = name
-    def __repr__(self):
-        return f"Guitarist instance. Name = {self.name}"
     def get_instrument(self):
         return f"guitar"
     def play_solo(self):
         return f"face melting guitar solo"
 
+
 class Bassist(Musician):
     def __init__(self, name):
         self.name = name
-    def __repr__(self):
-        return f"Bassist instance. Name = {self.name}"
     def get_instrument(self):
         return f"bass"
     def play_solo(self):
         return f"bom bom buh bom"
 
+
 class Drummer(Musician):
     def __init__ (self, name):
         self.name = name
-    def __repr__(self):
-        return f"Drummer instance. Name = {self.name}"
     def get_instrument(self):
         return f"drums"
     def play_solo(self):
--- tests/test_band.py
+++ tests/test_band.py
@@ -105,7 +105,6 @@ def test_band_members(one_band):
     assert one_band.members[2].name == "Dave Grohl"
 
 
-@pytest.mark.skip("pending")
 def test_play_solos_for_whole_band(one_band):
     solos = one_band.play_solos()
     assert len(solos) == 3
