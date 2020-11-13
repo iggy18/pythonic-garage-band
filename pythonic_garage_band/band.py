b540c5c02719dff99de4a7362c114da7a76e4cf0
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2020-11-12 17:59:15 -0800

    passing 11 tests. seemed like a good saving point

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,40 @@
+class Band:
+    def __init__(self, name, members=None):
+        self.name = name
+        self.members = members
+
+    def __str__(self):
+        return f"The band {self.name}"
+
+    def __repr__(self):
+        return f"Band instance. name={self.name}, members={self.members}"
+
+
+class Musician:
+    pass
+
+class Guitarist:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+    def get_instrument(self):
+        return f"guitar"
+
+class Bassist:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+class Drummer:
+    def __init__ (self, name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
