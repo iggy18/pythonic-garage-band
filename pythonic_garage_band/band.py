8cbbf00a04b22739837b05c6ec19f818e4e0590f
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2020-11-12 20:23:12 -0800

    16 tests passing, refactoring classes

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -2,43 +2,47 @@ class Band:
     def __init__(self, name, members=None):
         self.name = name
         self.members = members
-
     def __str__(self):
         return f"The band {self.name}"
-
     def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
 
-
 class Musician:
-    pass
-
-class Guitarist:
     def __init__(self, name):
         self.name = name
     def __str__(self):
-        return f"My name is {self.name} and I play guitar"
+        return f"My name is {self.name} and I play {self.get_instrument()}"
+    def get_instrument(self):
+        return f"bleh"
+
+
+class Guitarist(Musician):
+    def __init__(self, name):
+        self.name = name
     def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"
     def get_instrument(self):
         return f"guitar"
+    def play_solo(self):
+        return f"face melting guitar solo"
 
-class Bassist:
+class Bassist(Musician):
     def __init__(self, name):
         self.name = name
-    def __str__(self):
-        return f"My name is {self.name} and I play bass"
     def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
     def get_instrument(self):
         return f"bass"
+    def play_solo(self):
+        return f"bom bom buh bom"
 
-class Drummer:
+class Drummer(Musician):
     def __init__ (self, name):
         self.name = name
-    def __str__(self):
-        return f"My name is {self.name} and I play drums"
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
     def get_instrument(self):
         return f"drums"
+    def play_solo(self):
+        return f"rattle boom crash"
+    
--- tests/test_band.py
+++ tests/test_band.py
@@ -33,8 +33,6 @@ def test_guitarist_repr():
     expected = "Guitarist instance. Name = Joan Jett"
     assert actual == expected
 
-
-
 def test_drummer_str():
     sheila = Drummer("Sheila E.")
     actual = str(sheila)
@@ -47,14 +45,11 @@ def test_drummer_repr():
     expected = "Drummer instance. Name = Sheila E."
     assert actual == expected
 
-
-
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
     expected = "My name is Meshell Ndegeocello and I play bass"
     assert actual == expected
-
 
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
@@ -62,34 +57,27 @@ def test_bassist_repr():
     expected = "Bassist instance. Name = Meshell Ndegeocello"
     assert actual == expected
 
-
 def test_guitarist():
     jimi = Guitarist("Jimi Hendrix")
     assert jimi.name == "Jimi Hendrix"
     assert jimi.get_instrument() == "guitar"
-
 
 def test_bassist():
     flea = Bassist("Flea")
     assert flea.name == "Flea"
     assert flea.get_instrument() == "bass"
 
-
 def test_drummer():
     ginger = Drummer("Ginger Baker")
     assert ginger.name == "Ginger Baker"
     assert ginger.get_instrument() == "drums"
 
-
-@pytest.mark.skip("pending")
 def test_instruments(one_band):
     instruments = ["guitar", "bass", "drums"]
     for i, member in enumerate(one_band.members):
         # NOTE: see stretch goal where zip used
         assert member.get_instrument() == instruments[i]
 
-
-@pytest.mark.skip("pending")
 def test_individual_solos(one_band):
     for member in one_band.members:
         if member.get_instrument() == "guitar":
@@ -100,7 +88,6 @@ def test_individual_solos(one_band):
             assert member.play_solo() == "rattle boom crash"
 
 
-@pytest.mark.skip("pending")
 def test_band_members(one_band):
 
     assert len(one_band.members) == 3
